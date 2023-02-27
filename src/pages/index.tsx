9953d6b61d99dbef47d105ebc15e8cef5b7d2c80
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-02-27 17:01:06 +0000

    Project Setup

---
+++ src/pages/index.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from 'next/font/google'
+import styles from '@/styles/Home.module.css'
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <h3>fcc-react-nextjs-firebase-chakra-reddit-app</h3>
+    </>
+  )
+}
