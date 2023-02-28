417204c1faff698048a44710219997ead1a7330a
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-02-28 14:42:24 +0000

    Navbar initial

---
+++ src/components/Navbar/Navbar.tsx
@@ -0,0 +1,24 @@
+import { Flex, Image } from '@chakra-ui/react'
+import React from 'react'
+
+type NavbarProps = {}
+
+const Navbar: React.FC = () => {
+  return (
+    <Flex bg="white" height="44px" padding="6px 12px">
+      <Flex align="center">
+        <Image src="/images/redditFace.svg" alt="reddit image" height="30px" />
+        <Image
+          src="/images/redditText.svg"
+          alt="the reddit logo"
+          height="46px"
+          display={{ base: 'none', md: 'unset' }}
+        />
+      </Flex>
+      {/* <Direction/>
+      <SearchInput/>
+      <RightContent/> */}
+    </Flex>
+  )
+}
+export default Navbar
