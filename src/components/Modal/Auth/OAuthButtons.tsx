155d7a6577a2d9c1ec8b154e39d8869c2e4e9c36
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-03-07 14:31:28 +0000

    Login with Google

--- src/components/Modal/Auth/OAuthButtons.tsx
+++ src/components/Modal/Auth/OAuthButtons.tsx
@@ -1,10 +1,18 @@
-import { Button, Flex, Image } from '@chakra-ui/react'
+import { Button, Flex, Image, Text } from '@chakra-ui/react'
 import React from 'react'
+import { useSignInWithGoogle } from 'react-firebase-hooks/auth'
+import { auth } from '../../../firebase/clientApp'
 
 const OAuthButtons: React.FC = () => {
+  const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth)
   return (
     <Flex direction="column" width="100%" mb={4}>
-      <Button variant="oauth" mb={2} mt={2}>
+      <Button
+        variant="oauth"
+        mb={2}
+        mt={2}
+        isLoading={loading}
+        onClick={() => signInWithGoogle()}>
         <Image src="/images/googlelogo.png" height="20px" mr={2} />
         Continue with Google
       </Button>
@@ -13,6 +21,7 @@ const OAuthButtons: React.FC = () => {
         <Image src="/images/microsoftlogo.svg" height="20px" mr={2} />
         Continue with Microsoft
       </Button>
+      {error && <Text> {error.message}</Text>}
     </Flex>
   )
 }
