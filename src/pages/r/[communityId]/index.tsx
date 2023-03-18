7826ec4d384eb30bf22e2ea6f409eccd183f3f2c
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-03-18 15:05:03 +0000

    Creating communities & 404 page

---
+++ src/pages/r/[communityId]/index.tsx
@@ -0,0 +1,46 @@
+import { Community } from '../../../atoms/communitiesAtom'
+import { doc, getDoc } from 'firebase/firestore'
+import { GetServerSidePropsContext } from 'next'
+import React from 'react'
+import safeJsonStringify from 'safe-json-stringify'
+import { firestore } from '../../../firebase/clientApp'
+import NotFound from '../../../components/Community/NotFound'
+
+type CommunityPageProps = {
+  communityData: Community
+}
+
+const CommunityPage: React.FC<CommunityPageProps> = ({ communityData }) => {
+  console.log('here is the data', communityData)
+
+  if (!communityData) {
+    return <NotFound />
+  }
+  return <div>Welcome to {communityData.id}</div>
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  // get community data and pass it to the client
+  try {
+    const communityDocRef = doc(
+      firestore,
+      'communities',
+      context.query.communityId as string
+    )
+    const communityDoc = await getDoc(communityDocRef)
+
+    return {
+      props: {
+        communityData: communityDoc.exists()
+          ? JSON.parse(
+              safeJsonStringify({ id: communityDoc.id, ...communityDoc.data() })
+            )
+          : '',
+      },
+    }
+  } catch (error) {
+    // Possible to add an error page here
+    console.log('GetServersideProps error', error)
+  }
+}
+export default CommunityPage
