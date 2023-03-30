0a8501a2c899e69b96c5a1bf23792671ae105860
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-03-30 14:52:41 +0100

    Creating the new post object

--- src/components/Posts/NewPostForm.tsx
+++ src/components/Posts/NewPostForm.tsx
@@ -1,5 +1,6 @@
 import { Flex, Icon } from '@chakra-ui/react'
 import React, { useState } from 'react'
+import { useRouter } from 'next/router'
 import { BiPoll } from 'react-icons/bi'
 import { BsLink45Deg, BsMic } from 'react-icons/bs'
 import { IoDocumentText, IoImageOutline } from 'react-icons/io5'
@@ -7,8 +8,14 @@ import { AiFillCloseCircle } from 'react-icons/ai'
 import TabItem from './TabItem'
 import TextInputs from './PostForm/TextInputs'
 import ImageUpload from './PostForm/ImageUpload'
+import { Post } from '@/src/atoms/postsAtom'
+import { User } from 'firebase/auth'
+import { serverTimestamp, Timestamp } from 'firebase/firestore'
 
-type NewPostFormProps = {}
+type NewPostFormProps = {
+  // user?: User | null
+  user: User
+}
 
 const formTabs: TabItem[] = [
   {
@@ -38,13 +45,33 @@ export type TabItem = {
   icon: typeof Icon.arguments
 }
 
-const NewPostForm: React.FC<NewPostFormProps> = () => {
+const NewPostForm: React.FC<NewPostFormProps> = ({ user }) => {
+  const router = useRouter()
   const [selectedTab, setSelectedTab] = useState(formTabs[0].title)
   const [loading, setLoading] = useState(false)
   const [textInputs, setTextInputs] = useState({ title: '', body: '' })
   const [selectedFile, setSelectedFile] = useState<string>()
 
-  const handleCreatePost = async () => {}
+  const handleCreatePost = async () => {
+    const { communityId } = router.query
+
+    // create the new post object => type Post
+    const newPost: Post = {
+      communityId: communityId as string,
+      creatorId: user?.uid,
+      creatorDisplayName: user.email!.split('@')[0],
+      title: textInputs.title,
+      body: textInputs.body,
+      numberOfComments: 0,
+      voteStatus: 0,
+      createdAt: serverTimestamp() as Timestamp,
+    }
+    // store the post in database
+    // check for selectedFile
+    // store in storage => getDownloadURL (return imageURL)
+    // update post doc by adding imageURL
+    // redirect the user back to the communityPage using the router
+  }
 
   const onSelectImage = async (event: React.ChangeEvent<HTMLImageElement>) => {
     const reader = new FileReader()
--- src/pages/r/[communityId]/submit.tsx
+++ src/pages/r/[communityId]/submit.tsx
@@ -2,14 +2,17 @@ import { Box, Text } from '@chakra-ui/react'
 import React from 'react'
 import PageContent from '../../../components/Layout/PageContent'
 import NewPostForm from '../../../components/Posts/NewPostForm'
+import { useAuthState } from 'react-firebase-hooks/auth'
+import { auth } from '@/src/firebase/clientApp'
 const SubmitPostPage: React.FC = () => {
+  const [user] = useAuthState(auth)
   return (
     <PageContent>
       <>
         <Box p="14px 0px" borderBottom="1px solid" borderColor="white">
           <Text>Create a post</Text>
         </Box>
-        <NewPostForm />
+        {user && <NewPostForm user={user} />}
       </>
       <>{/* About */}</>
     </PageContent>
