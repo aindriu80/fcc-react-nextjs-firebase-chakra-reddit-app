901434f8017f7c306e4996d5b83d6bf7f8db5063
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-04-12 14:39:49 +0100

    Toggling the Menu via  click on the menu down arrow

--- src/components/Navbar/Directory/Directory.tsx
+++ src/components/Navbar/Directory/Directory.tsx
@@ -3,16 +3,20 @@ import { Flex, Icon, Menu, MenuButton, MenuList, Text } from '@chakra-ui/react'
 import React from 'react'
 import { TiHome } from 'react-icons/ti'
 import Communities from './Communities'
+import useDirectory from '@/src/hooks/useDirectory'
 const Directory: React.FC = () => {
+  const { directoryState, toggleMenuOpen } = useDirectory()
+
   return (
-    <Menu>
+    <Menu isOpen={directoryState.isOpen}>
       <MenuButton
         cursor="pointer"
         padding="0px 6px"
         borderRadius={4}
         mr={2}
         ml={{ base: 0, md: 2 }}
-        _hover={{ outline: '1px solid', outlineColor: 'grey.200' }}>
+        _hover={{ outline: '1px solid', outlineColor: 'grey.200' }}
+        onClick={toggleMenuOpen}>
         <Flex
           align="center"
           justify="space-between"
---
+++ src/hooks/useDirectory.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import { useRecoilState } from 'recoil'
+import { directoryMenuState } from '../atoms/directoryMenuAtom'
+
+const useDirectory = () => {
+  const [directoryState, setDirectoryState] = useRecoilState(directoryMenuState)
+
+  const toggleMenuOpen = () => {
+    setDirectoryState((prev) => ({
+      ...prev,
+      isOpen: !directoryState.isOpen,
+    }))
+  }
+
+  return { directoryState, toggleMenuOpen }
+}
+export default useDirectory
